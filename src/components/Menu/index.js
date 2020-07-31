39fd63c6b1fe8adf12318a311adf99ecba613f3f
Rita Lino <rita.rlino@gmail.com>
2020-07-31 15:56:23 -0300

    initial setup

---
+++ src/components/Menu/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Logo from "../../assets/img/Logo.png";
+import "./Menu.css";
+import Button from "../Button";
+
+function Menu() {
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="Emoflix Logo" />
+      </a>
+
+      <Button as="a" href="/" className="ButtonLink">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
+}
+
+export default Menu;
